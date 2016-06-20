ea5502e680d522b4b515d49f0df1f1a05b293aa9
Moon243 <Gideonssolock@gmail.com>
2016-06-20 12:54:42 -0700

    AddedLoad/Stop

--- src/me/soringaming/moon/korra/GeyserRush/GeyserRush.java
+++ src/me/soringaming/moon/korra/GeyserRush/GeyserRush.java
@@ -1,76 +1,86 @@
 package me.soringaming.moon.korra.GeyserRush;
 
+import java.util.logging.Level;
+
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
+import org.bukkit.permissions.Permission;
+import org.bukkit.permissions.PermissionDefault;
+import org.bukkit.util.Vector;
 
+import com.projectkorra.projectkorra.ProjectKorra;
 import com.projectkorra.projectkorra.ability.AddonAbility;
 import com.projectkorra.projectkorra.ability.WaterAbility;
 
 public class GeyserRush extends WaterAbility implements AddonAbility {
-
+	
+	private Player player;
+	private Location loc;
+	private Vector dir;
+	private Permission perm;
+	
 	public GeyserRush(Player player) {
 		super(player);
-		// TODO Auto-generated constructor stub
+		this.player = player;
+		this.loc = player.getLocation();
+		this.dir = player.getEyeLocation().getDirection().normalize().multiply(1);
+		start();
 	}
 
 	@Override
 	public long getCooldown() {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
 	public Location getLocation() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public String getName() {
-		// TODO Auto-generated method stub
-		return null;
+		return "GeyserRush";
 	}
 
 	@Override
 	public boolean isHarmlessAbility() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean isSneakAbility() {
-		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 	@Override
 	public void progress() {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public String getAuthor() {
-		// TODO Auto-generated method stub
-		return null;
+		return "Soringaming & Moon243";
 	}
 
 	@Override
 	public String getVersion() {
-		// TODO Auto-generated method stub
-		return null;
+		return "v1.0";
 	}
 
 	@Override
 	public void load() {
-		// TODO Auto-generated method stub
-		
+		ProjectKorra.plugin.getServer().getLogger().log(Level.INFO, getName() + " " + getVersion() + " Developed By " + getAuthor() + " Has Been Enabled" );
+		ProjectKorra.plugin.getServer().getPluginManager().registerEvents(new GeyserRushListener(), ProjectKorra.plugin);
+		perm = new Permission("bending.ability.GeyserRush");
+		perm.setDefault(PermissionDefault.TRUE);
+		ProjectKorra.plugin.getServer().getPluginManager().addPermission(perm);
 	}
 
 	@Override
 	public void stop() {
-		// TODO Auto-generated method stub
-		
+		ProjectKorra.plugin.getServer().getLogger().log(Level.INFO, getName() + " " + getVersion() + " Developed By " + getAuthor() + " Has Been Disabled" );
+		ProjectKorra.plugin.getServer().getPluginManager().removePermission(perm);
+		super.remove();
 	}
 
 }
