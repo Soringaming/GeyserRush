4fa3748acd5b73a37298d4c9c42c3123f9cd995b
Soringaming <thehawksquad50@gmail.com>
2016-06-21 05:54:50 +1000

    Started source selection

--- src/me/soringaming/moon/korra/GeyserRush/GeyserRush.java
+++ src/me/soringaming/moon/korra/GeyserRush/GeyserRush.java
@@ -1,16 +1,31 @@
 package me.soringaming.moon.korra.GeyserRush;
 
 import org.bukkit.Location;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 
+import com.projectkorra.projectkorra.GeneralMethods;
 import com.projectkorra.projectkorra.ability.AddonAbility;
 import com.projectkorra.projectkorra.ability.WaterAbility;
+import com.projectkorra.projectkorra.util.BlockSource;
+import com.projectkorra.projectkorra.util.ClickType;
 
 public class GeyserRush extends WaterAbility implements AddonAbility {
+	
+	private Block sourceBlock;
+	private boolean sourceSelected;
 
 	public GeyserRush(Player player) {
 		super(player);
 		// TODO Auto-generated constructor stub
+	}
+	
+	public void selectSource() {
+		sourceBlock = BlockSource.getWaterSourceBlock(player, 15, ClickType.SHIFT_DOWN, true, true, false, false, false);
+		if (sourceBlock != null && !GeneralMethods.isRegionProtectedFromBuild(this, sourceBlock.getLocation())) {
+			sourceSelected = true;
+			start();
+		}
 	}
 
 	@Override
--- src/me/soringaming/moon/korra/GeyserRush/GeyserRushListener.java
+++ src/me/soringaming/moon/korra/GeyserRush/GeyserRushListener.java
@@ -1,7 +1,14 @@
 package me.soringaming.moon.korra.GeyserRush;
 
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerToggleSneakEvent;
 
 public class GeyserRushListener implements Listener {
+	
+	@EventHandler
+	public void selectSource(PlayerToggleSneakEvent e) {
+		
+	}
 
 }
