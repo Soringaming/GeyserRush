f4bec3d9a5fd46ee75a957cae436cc948b20d9ef
Soringaming <thehawksquad50@gmail.com>
2016-06-21 06:34:02 +1000

    I has started le reverting methods ^0^

--- src/me/soringaming/moon/korra/GeyserRush/GeyserRushListener.java
+++ src/me/soringaming/moon/korra/GeyserRush/GeyserRushListener.java
@@ -1,14 +1,32 @@
 package me.soringaming.moon.korra.GeyserRush;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerToggleSneakEvent;
+import org.bukkit.event.player.PlayerAnimationEvent;
+
+import com.projectkorra.projectkorra.BendingPlayer;
+import com.projectkorra.projectkorra.ability.CoreAbility;
 
 public class GeyserRushListener implements Listener {
 	
 	@EventHandler
-	public void selectSource(PlayerToggleSneakEvent e) {
+	public void selectSource(PlayerAnimationEvent e) {
+		Player p = e.getPlayer();
+		if(canBend(p)) {
+			new GeyserRush(p);
+		}
 		
 	}
+	
+	public boolean canBend(Player player) {
+		BendingPlayer bp = BendingPlayer.getBendingPlayer(player.getName());
+		if(bp.canBend(CoreAbility.getAbility("GeyserRush"))) {
+			return true;
+		}
+		return false;
+	}
+	
+	
 
 }
